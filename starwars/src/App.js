1297ff85a893f4ff1e959dd1ae30b37498f866fa
Clark Williams <theclarkwilliams@gmail.com>
2019-05-31 11:24:42 -0600

    added some neat styling, trying pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -24,12 +24,15 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
+        console.log(data);
         this.setState({ starwarsChars: data.results });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
+
+  
 
   render() {
     return (
