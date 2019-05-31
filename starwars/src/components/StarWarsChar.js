7e8dd89f5024e087db7372a7278e261d658ce33e
Clark Williams <theclarkwilliams@gmail.com>
2019-05-31 12:08:05 -0600

    goin to lunch, done for sprint challenge time

--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -7,18 +7,18 @@ const StarWarsChar = props => {
             <div className="chars">
                 <h2>{props.charName}</h2>
                 <ul>
+                    <li><b>Gender:</b> {props.gender}</li>
                     <li><b>Birth Year:</b> {props.birthYear}</li>
                     <li><b>Eye Color:</b> {props.eyeColor}</li>
-                    {/* <li><b>Films:</b> {props.films}</li> */}
-                    <li><b>Gender:</b> {props.gender}</li>
                     <li><b>Hair Color:</b> {props.hairColor}</li>
-                    <li><b>Height(cm):</b> {props.height}</li>
-                    {/* <li><b>Homeworld:</b> {props.homeworld}</li> */}
-                    <li><b>Mass(kg):</b> {props.mass}</li>
                     <li><b>Skin Color:</b> {props.skinColor}</li>
-                    {/* <li><b>Species:</b> {props.species}</li>
+                    <li><b>Height(cm):</b> {props.height}</li>
+                    <li><b>Mass(kg):</b> {props.mass}</li>
+                    {/* <li><b>Homeworld:</b> {props.homeworld}</li>
+                    <li><b>Species:</b> {props.species}</li>
                     <li><b>Starships:</b> {props.starships}</li>
                     <li><b>Vehicles:</b> {props.vehicles}</li> */}
+                    {/* <li><b>Films:</b> {props.films}</li> */}
                 </ul>
             </div>
         </div>
